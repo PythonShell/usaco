670ae1f4645d84b26ad32aa03bde1546ce313b2e
PythonShell <pythonshell@yeah.net>
2016-05-24 21:13:20 +0800

    [Partial] Add read data statements for lamps prob
    Signed-off-by: PythonShell <pythonshell@yeah.net>

---
+++ lamps.cpp
@@ -0,0 +1,34 @@
+/*
+ID: pythons1
+PROG: lamps
+LANG: C++
+*/
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+#define DEBUG
+
+int main() {
+	ofstream fout("lamps.out");
+	ifstream fin("lamps.in");
+    int n, c;
+    fin >> n >> c;
+    int tmp;
+    unsigned long long onConfig=0;
+    unsigned long long offConfig=0;
+    while(1) {
+        fin >> tmp;
+        if(tmp<0) break;
+        onConfig |= (unsigned long long(1) << (tmp-1));
+    }
+    while(1) {
+        fin >> tmp;
+        if(tmp<0) break;
+        offConfig |= (unsigned long long(1) << (tmp-1));
+    }
+#ifdef DEBUG
+    cout << onConfig << endl << offConfig << endl;
+#endif
+	return 0;
+}
