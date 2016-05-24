f1f722c50074ad4932dbd7e95fdd2195093e1a2b
PythonShell <pythonshell@yeah.net>
2016-05-25 01:16:40 +0800

    [Success] use stupid method
    list all possible situations, then print them out.
    Signed-off-by: PythonShell <pythonshell@yeah.net>

--- lamps.cpp
+++ lamps.cpp
@@ -5,6 +5,7 @@ LANG: C++11
 */
 #include<iostream>
 #include<fstream>
+#include<string>
 #include<vector>
 #include<algorithm>
 using namespace std;
@@ -22,116 +23,71 @@ using namespace std;
 int main() {
     ofstream fout("lamps.out");
     ifstream fin("lamps.in");
+    vector<string> situations;
+    situations.push_back(string("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"));
+    situations.push_back(string("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"));
+    situations.push_back(string("0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101"));
+    situations.push_back(string("1010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010"));
+    situations.push_back(string("0110110110110110110110110110110110110110110110110110110110110110110110110110110110110110110110110110"));
+    situations.push_back(string("1001001001001001001001001001001001001001001001001001001001001001001001001001001001001001001001001001"));
+    situations.push_back(string("1100011100011100011100011100011100011100011100011100011100011100011100011100011100011100011100011100"));
+    situations.push_back(string("0011100011100011100011100011100011100011100011100011100011100011100011100011100011100011100011100011"));
     int n, c;
     fin >> n >> c;
-    unsigned __int128 filter = (unsigned __int128)(1) << (n-1);
-    unsigned __int128 OPER_1=0;
-    OPER_1 = ~OPER_1;
-    unsigned __int128 OPER_2=0;
-    for(int i=0; i<64; ++i) {
-        OPER_2 |= ((unsigned __int128)(1) << (i*2));
+    for(int i=0; i<8; ++i) {
+        situations[i].resize(n);
     }
-    unsigned __int128 OPER_3=0;
-    OPER_3 = ~OPER_2;
-    unsigned __int128 OPER_4=0;
-    for(int i=0; i<40; ++i) {
-        OPER_4 |= ((unsigned __int128)(1) << (i*3));
-    }
-#ifdef DEBUG
-    unsigned __int128 tt = OPER_1;
-    for(int i=0; i<128; ++i) {
-        if(tt & 1) cout << 1;
-        else cout << 0;
-        tt = tt >> 1;
-    } cout << endl;
-    tt = OPER_2;
-    for(int i=0; i<128; ++i) {
-        if(tt & 1) cout << 1;
-        else cout << 0;
-        tt = tt >> 1;
-    } cout << endl;
-    tt = OPER_3;
-    for(int i=0; i<128; ++i) {
-        if(tt & 1) cout << 1;
-        else cout << 0;
-        tt = tt >> 1;
-    } cout << endl;
-    tt = OPER_4;
-    for(int i=0; i<128; ++i) {
-        if(tt & 1) cout << 1;
-        else cout << 0;
-        tt = tt >> 1;
-    } cout << endl;
-#endif
     int tmp;
-    unsigned __int128 onConfig=0;
-    unsigned __int128 offConfig=0;
+    vector<int> onConfig;
+    vector<int> offConfig;
     while(1) {
         fin >> tmp;
         if(tmp<0) break;
-        onConfig |= ((unsigned __int128)(1) << (tmp-1));
+        onConfig.push_back(tmp);
     }
     while(1) {
         fin >> tmp;
         if(tmp<0) break;
-        offConfig |= ((unsigned __int128)(1) << (tmp-1));
+        offConfig.push_back(tmp);
     }
-    unsigned __int128 initial = 0;
-    initial = ~initial;
-    vector<unsigned __int128> result;
+    fin.close();
     if(c==0) {
-        result.push_back(initial);
+        situations.resize(1);
     }
     else if (c==1) {
-        result.push_back(initial ^ OPER_1);
-        result.push_back(initial ^ OPER_2);
-        result.push_back(initial ^ OPER_3);
-        result.push_back(initial ^ OPER_4);
+        situations.resize(5);
+        situations.erase(situations.begin());
     }
-    else if( c==2) {
-        result.push_back(initial);
-        result.push_back(initial ^ OPER_1);
-        result.push_back(initial ^ OPER_2);
-        result.push_back(initial ^ OPER_3);
-        result.push_back((initial ^ OPER_1) ^ OPER_4);
-        result.push_back((initial ^ OPER_2) ^ OPER_4);
-        result.push_back((initial ^ OPER_3) ^ OPER_4);
-    } else {
-        result.push_back(initial);
-        result.push_back(initial ^ OPER_1);
-        result.push_back(initial ^ OPER_2);
-        result.push_back(initial ^ OPER_3);
-        result.push_back(initial ^ OPER_4);
-        result.push_back((initial ^ OPER_1) ^ OPER_4);
-        result.push_back((initial ^ OPER_2) ^ OPER_4);
-        result.push_back((initial ^ OPER_3) ^ OPER_4);
+    else if (c==2) {
+        situations.erase(situations.begin()+4);
     }
-    vector<unsigned __int128> final_result;
-    for(int j=0; j<result.size(); ++j) {
-        unsigned __int128 current = result[j];
-        if(~current & onConfig) continue;
-        if(current & offConfig) continue;
-        unsigned __int128 tmp = 0;
-        for(int i=0; i<n; ++i) {
-            if(current & 1) tmp = (tmp << 1) + 1;
-            else tmp = tmp << 1;
-            current = current >> 1;
+    for(int i=situations.size()-1; i>-1; --i) {
+        bool flag = false;
+        for(int j=0; j<onConfig.size(); ++j) {
+            if(situations[i][onConfig[j]-1]!='1') {
+                flag = true;
+                break;
+            }
         }
-        final_result.push_back(tmp);
+        for(int j=0; j<offConfig.size(); ++j) {
+            if(situations[i][offConfig[j]-1]!='0') {
+                flag = true;
+                break;
+            }
+        }
+        if(flag) {
+            situations.erase(situations.begin()+i);
+        }
     }
-    if(final_result.size()==0) {
+    if(situations.size()==0) {
         fout << "IMPOSSIBLE" << endl;
         return 0;
     }
-    sort(final_result.begin(), final_result.end());
-    for(int j=0; j<final_result.size(); ++j) {
-        unsigned __int128 current = final_result[j];
-        for(int i=0; i<n; ++i) {
-            if(current & filter) fout << 1;
-            else fout << 0;
-            current = current << 1;
-        } fout << endl;
+    sort(situations.begin(), situations.end());
+    for(int j=0; j<situations.size(); ++j) {
+        fout << situations[j] << endl;
     }
+    fout.close();
     return 0;
 }
 
